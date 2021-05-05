95f1c572f68f9e4d4f579cb8edf30cdb5059d8a1
Ejez <ejez@protonmail.ch>
2021-05-05 11:26:32 +0300

    docs: Change vuepress bundler to vite.

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,10 +1,19 @@
-import type { DefaultThemeOptions } from 'vuepress'
-import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions, ViteBundlerOptions } from 'vuepress-vite'
+import { defineUserConfig } from 'vuepress-vite'
 
-export default defineUserConfig<DefaultThemeOptions>({
+export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
   lang: 'en',
   title: 'Vint',
   description: 'Vint, easy i18n for your Vue app.',
 
   dest: 'docs/dist',
+
+  bundler: '@vuepress/vite',
+  bundlerConfig: {
+    viteOptions: {
+      build: {
+        emptyOutDir: true,
+      },
+    },
+  },
 })
