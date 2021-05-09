5c19df2b4ba6793a0ba0ba168b4c49e84363bf21
Ejez <ejez@protonmail.ch>
2021-05-09 19:55:51 +0300

    testing: Make ts-jest process js files.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
+  // Needed to correctly handle ES modules in js files.
+  // "allowJs" also needs to be set to "true" in "tsconfig.json".
+  preset: 'ts-jest/presets/js-with-ts',
+  transformIgnorePatterns: ['node_modules/(?!(@pyxo/wint)/)'],
+
   testEnvironment: 'node',
   moduleNameMapper: {
     '^vint$': '<rootDir>/src',
